87c6f0058ce65933b500c5f73c2472a2082c86ec
n-danilova <apple.for.ledi@icloud.com>
2021-11-26 17:56:58 +0300

    upd1.2

--- src/LabN2.java
+++ src/LabN2.java
@@ -5,12 +5,12 @@ import pokemons.*;
 public class LabN2 {
     public static void main(String[] args) {
         Battle battle = new Battle();
-        Eevee p1 = new Eevee("Мохнатый пикачу",2);
-        Sylveon p2 = new Sylveon("Стилевый пикачу", 3);
-        TapuLele p3 = new TapuLele("Анимэшница",1);
-        Gothita p4 = new Gothita("ГОТита",5);
-        Gothorita p5 = new Gothorita("Шестерка ГОТиты",4);
-        Gothitelle p6 = new Gothitelle("Ноунейм",6);
+        Eevee p1 = new Eevee("Еевее",2);
+        Sylveon p2 = new Sylveon("Сульвеон", 3);
+        TapuLele p3 = new TapuLele("Тапулеле",1);
+        Gothita p4 = new Gothita("Готита",5);
+        Gothorita p5 = new Gothorita("Шестерка Готиты(Готорита)",4);
+        Gothitelle p6 = new Gothitelle("Шестрека готориты (Готителль)",6);
         battle.addAlly(p6);
         battle.addAlly(p5);
         battle.addAlly(p3);
